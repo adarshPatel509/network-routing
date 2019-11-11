7124af77664b179e1951c045f2068c78cf8ea036
Adarsh Patel <adarshpatel509@gmail.com>
2019-11-11 15:19:11 +0530

    Add file transfer between nodes

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
-var app = require('express')();
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
-var path = require('path');
+let express = require('express');
+let app = express();
+let http = require('http').createServer(app);
+let io = require('socket.io')(http);
+let path = require('path');
+let ss = require('socket.io-stream');
+let fs = require('fs');
+let lineReader = require('line-reader');
+
+app.use(express.static('public'))
 
 app.get('/*', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -13,6 +19,30 @@ let routingTable = "";
 
 io.on('connection', function(socket){
   console.log('New Connection!!');
+  
+  //handeling fileUpload
+  ss(socket).on('sendFile', function(stream, data) {
+    console.log("Recieved file ", data.name, " from ", data.from, " to send at node ", data.to);
+    var filename = path.basename(data.name);
+    stream.pipe(fs.createWriteStream(filename))
+    .on('finish', function() {
+       //sendFile to destination node line by line
+        lineReader.eachLine(data.name, function(line, last) {
+        console.log(line, "\n");
+        let message = {message: line, from: data.from}
+        if(cleintsMap[data.to] == undefined) {
+          console.log("Destination node ", to ," not found");
+        } 
+        else {
+          cleintsMap[data.to].socket.emit('messageFromServer', message);
+          if(last) {
+            console.log("File send to destination node successfully!");
+          }
+        }
+      });
+    });
+  });
+
   //handle client echo message
   socket.on("helloFromClient", function(nodeName, nodeDistance) {
       cleintsMap[nodeName] = {socket: socket, distance: nodeDistance};
